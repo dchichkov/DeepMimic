6c0fde2c9ad96b7ee32bac45f9e29330d0f9dab6
Dmitry Chichkov <dchichkov@gmail.com>
2019-02-09 22:09:08 -0800

    checkpoint - builds/runs @desk

---
+++ env.sh
@@ -0,0 +1,3 @@
+export PYTHONPATH=/home/dmitry/github/bullet3.regular/build_cmake/examples/pybullet:$PYTHONPATH
+export PYTHONPATH=/home/dmitry/github/bullet3.regular/examples/pybullet/gym:/home/dmitry/github/gym:$PYTHONPATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/dmitry/github/bullet3.regular/build_cmake/src/BulletDynamics:/home/dmitry/github/bullet3.regular/build_cmake/src/BulletCollision:/home/dmitry/github/bullet3.regular/build_cmake/src/LinearMath:/home/dmitry/17DOF/DeepMimic/DeepMimicCore
